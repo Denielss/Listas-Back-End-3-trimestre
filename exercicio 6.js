75d726a05d6aa737610ee556124710d825e66e97
Denielss <daniel.martin@escola.pr.gov.br>
2024-10-20 22:47:56 -0300

    fizz ate aoito
    eh isso , travei na oito e deu sono, vo durmi

--- exercicio 6.js
+++ exercicio 6.js
@@ -1,3 +1,5 @@
+let codigoA = prompt("Digite o codigo do aluno : ");
+
 let nota1 = prompt("Digite a primeira nota : ");
 
 let nota2 = prompt("Digite a segunda nota : ");
@@ -14,6 +16,8 @@ let soma = (nota1+nota2+nota3);
 
 let media = soma / 3;
 
+console.log("COD Aluno : " + codigoA);
+
 console.log("nota 1 = " + nota1);
 
 console.log("nota 2 = " + nota2);
@@ -22,7 +26,7 @@ console.log("nota 3 = " + nota3);
 
 console.log("A Média desse aluno eh : " + media);
 
-if(media >= 60){
+if(media >= 50){
 
     console.log("Aprovado !!!");
 
---
+++ exercicio 8.js
@@ -0,0 +1,9 @@
+console.log("!! por favor faça seu pedido ultilizando os codigos do produtos !!");
+console.log("Especificação   Código  Preço");
+console.log("Cachorro quente  100     1,20");
+console.log("Bauru simples    101     1,30");
+console.log("Bauru com ovo    102     1,50");
+console.log("Hambúrger        103     1,20");
+console.log("Cheeseburguer    104     1,30");
+console.log("Refrigerante     105     1,00");
+
